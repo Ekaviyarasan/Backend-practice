7a591b069a3b8b3c7d0c0665afd63d0c235628e4
Kaviarasan E <144410585+Ekaviyarasan@users.noreply.github.com>
2026-03-12 17:02:19 +0530

    Update MemberThree.jsx

--- react2/vite/com/project/src/pages/MemberThree.jsx
+++ react2/vite/com/project/src/pages/MemberThree.jsx
@@ -2,7 +2,12 @@ import MemberContent from "../Component/MemberContent";
 
 function MemberThree() {
   return (
-    <MemberContent title="Member Three Experience" />
+    <MemberContent
+      title="Importance of Facilities for Student Development"
+      problem={`Students need more than classroom learning to grow and prepare for their future careers. In some cases, colleges may have limited facilities such as workshops, technical events, or industry exposure. Without these opportunities, students may not fully develop practical skills, communication abilities, or teamwork experience.`}
+
+      solution={`Providing better facilities such as skill development workshops, educational tours, technical clubs, and cultural activities can greatly benefit students. These opportunities allow students to explore their interests, gain real-world knowledge, and develop important life skills. Improved facilities also enhance the reputation of the college and help students achieve better career opportunities and placements in the future.`}
+    />
   );
 }
 
